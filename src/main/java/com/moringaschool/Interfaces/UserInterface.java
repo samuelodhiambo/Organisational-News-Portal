aa2717676a68b33163bbb8ba4950203a19e88378
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-13 23:56:08 +0300

    patch: It will create User Interface

---
+++ src/main/java/com/moringaschool/Interfaces/UserInterface.java
@@ -0,0 +1,20 @@
+package com.moringaschool.Interfaces;
+
+import com.moringaschool.Models.User;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public interface UserInterface {
+    void add(User user);
+
+    List<User> findAll(Connection conn);
+
+    User findById(int id);
+
+    void update(int id);
+
+    void deleteById(int id);
+
+    void clearAll();
+}
