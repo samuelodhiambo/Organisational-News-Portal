8833365782ef3892b85ea7674e0dc1b04a44895c
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-14 00:04:52 +0300

    patch: It will create Department DAO implementing Department Interface

---
+++ src/main/java/com/moringaschool/Dao/DepartmentDao.java
@@ -0,0 +1,46 @@
+package com.moringaschool.Dao;
+
+import com.moringaschool.Interfaces.DepartmentInterface;
+import com.moringaschool.Models.Department;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class DepartmentDao implements DepartmentInterface {
+    private final Sql2o sql2o;
+
+    public DepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Department department) {
+
+    }
+
+    @Override
+    public List<Department> findAll(Connection conn) {
+        return null;
+    }
+
+    @Override
+    public Department findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
+}
