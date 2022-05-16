e98504a74eb7d8b910360f649190cd5a17146ca9
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-16 09:48:14 +0300

    patch: It will add post route for adding user

--- src/main/java/com/moringaschool/Main.java
+++ src/main/java/com/moringaschool/Main.java
@@ -5,15 +5,25 @@ import com.moringaschool.Database.DB;
 import com.moringaschool.Models.Department;
 import com.moringaschool.Models.User;
 
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+
+
 public class Main {
     private static UserDao userDao = new UserDao(DB.sql2o);
     public static void main(String[] args) {
         DB.createTables(DB.sql2o.open());
-        Department department = new Department("ICT", "ICT support services", 20);
-//        System.out.println(department);
-        User user = new User("Samuel", "Manager", "operations", 1);
-//        System.out.println(user.getDepartment());
-        userDao.add(user);
-        System.out.println(user.getId());
+        Gson gson = new Gson();
+
+        post("/add/user", "application/json", (request, response) -> {
+            User user = gson.fromJson(request.body(), User.class);
+            userDao.add(user);
+            response.status(201);
+            response.type("application/json");
+            return gson.toJson(user);
+        });
+
+
     }
 }
